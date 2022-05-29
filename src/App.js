123774e1f5c92d94a790db16f5d402af1969c59b
SCartacci <sofia.cartacci@endava.com>
2022-05-29 19:59:00 -0300

    changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
-import React from "react";
+import React, { useState } from "react";
 import { ItemListContainer } from "./components/ItemListContainer/ItemListContainer";
 import Container from "./components/Container/Container";
 import HeroSlider from "./components/HeroSlider";
+import Counter from "./components/Container/Counter/Counter";
+import Counter2 from "./components/Counter2/Counter2";
+import Card from "./components/Card";
 
 function App() {
+  const [mostrar, setMostrar] = useState(true);
+  console.log(mostrar);
+
+  const mostrarCounter = () => {
+    setMostrar(!mostrar);
+  };
   return (
     <React.Fragment>
       <Navbar />
-      <Container>
-        <ItemListContainer />
-      </Container>
+      <HeroSlider></HeroSlider>
+      <ItemListContainer />
+      <Counter2 />
       <Container>
         <hr></hr>
-        <HeroSlider></HeroSlider>
-        <hr></hr>
+        <Card />
+        <Counter></Counter>
+        <button className="btn btn-primary" onClick={mostrarCounter}>
+          Mostrar/Ocultar
+        </button>
+        {mostrar ? "mostrando" : "Ocultando"}
       </Container>
     </React.Fragment>
   );
--- src/components/HeroSlider.js
+++ src/components/HeroSlider.js
@@ -1,45 +1,49 @@
 import React from "react";
 import { Carousel } from "react-bootstrap";
+import "../Styles/carousel.css";
 
 const HeroSlider = () => {
   return (
-    <Carousel>
-      <Carousel.Item interval={1000}>
-        <img
-          className="d-block w-100"
-          src="https://www.hogarmania.com/archivos/202010/como-lavar-ropa-de-color-668x400x80xX-1.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item interval={500}>
-        <img
-          className="d-block w-100"
-          src="https://www.hogarmania.com/archivos/202010/como-lavar-prendas-de-color-XxXx80.jpg"
-          alt="Second slide"
-        />
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="https://www.hogarmania.com/archivos/201911/ropa-listado-belleza-848x477x80xX.jpg"
-          alt="Third slide"
-        />
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
-    </Carousel>
+    <center>
+      {" "}
+      <Carousel>
+        <Carousel.Item interval={1000}>
+          <img
+            className="d-block w-100"
+            src="https://somosanimal.com/wp-content/uploads/2022/04/IMG_8265-scaled.jpg"
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item interval={500}>
+          <img
+            className="d-block w-100"
+            src="https://somosanimal.com/wp-content/uploads/2022/04/IMG_8268-scaled.jpg"
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src="https://somosanimal.com/wp-content/uploads/2022/04/IMG_8267-scaled.jpg"
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+            </p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </center>
   );
 };
 
